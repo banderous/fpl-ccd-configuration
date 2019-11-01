9637d8dc328271a7676ff0c72b849d67e25b6043
Adam Komisarek <adam.komisarek@hmcts.net>
2019-11-01 11:08:24 +0000

    Spike for case role and case title

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/ServiceTokenGeneratorConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/ServiceTokenGeneratorConfiguration.java
@@ -1,12 +1,17 @@
 package uk.gov.hmcts.reform.fpl.config;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import feign.Feign;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+
+import java.util.Map;
 
 @Configuration
 @Lazy
@@ -21,4 +26,26 @@ public class ServiceTokenGeneratorConfiguration {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 
+    public static void main(String[] args) {
+
+
+        String generate = new ServiceTokenGeneratorConfiguration().serviceAuthTokenGenerator("AABBCCDDEEFFGGHH", "fpl_case_service", new ServiceAuthorisationApi() {
+            @Override
+            public String serviceToken(Map<String, String> signIn) {
+                return new RestTemplate().postForEntity("http://localhost:4502/lease", signIn, String.class).getBody();
+            }
+
+            @Override
+            public void authorise(String authHeader, String[] roles) {
+
+            }
+
+            @Override
+            public String getServiceName(String authHeader) {
+                return null;
+            }
+        }).generate();
+
+        System.out.println(generate);
+    }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserService.java
@@ -6,12 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CaseAccessApi;
+import uk.gov.hmcts.reform.ccd.client.CaseUserApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseUser;
 import uk.gov.hmcts.reform.ccd.client.model.UserId;
 import uk.gov.hmcts.reform.fpl.config.LocalAuthorityUserLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.exceptions.NoAssociatedUsersException;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Service
 public class LocalAuthorityUserService {
@@ -21,19 +25,35 @@ public class LocalAuthorityUserService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final CaseAccessApi caseAccessApi;
+    private final CaseUserApi caseUserApi;
     private final LocalAuthorityUserLookupConfiguration localAuthorityUserLookupConfiguration;
     private final AuthTokenGenerator authTokenGenerator;
+    private final IdamClient client;
 
     @Autowired
     public LocalAuthorityUserService(CaseAccessApi caseAccessApi,
                                      LocalAuthorityUserLookupConfiguration localAuthorityUserLookupConfiguration,
-                                     AuthTokenGenerator authTokenGenerator) {
+                                     AuthTokenGenerator authTokenGenerator,
+                                     CaseUserApi caseUserApi,
+                                     IdamClient idamClient) {
         this.caseAccessApi = caseAccessApi;
         this.localAuthorityUserLookupConfiguration = localAuthorityUserLookupConfiguration;
         this.authTokenGenerator = authTokenGenerator;
+        this.caseUserApi = caseUserApi;
+        this.client = idamClient;
     }
 
     public void grantUserAccess(String authorization, String creatorUserId, String caseId, String caseLocalAuthority) {
+
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        var authentication = client.authenticateUser("fpl-system-update@mailnesia.com", "Password12");
+        caseUserApi.updateCaseRolesForUser(authentication, authTokenGenerator.generate(), caseId, creatorUserId,
+            new CaseUser(creatorUserId, Set.of("[LASOLICITOR]")));
+
         findUserIds(caseLocalAuthority).stream()
             .filter(userId -> !Objects.equals(userId, creatorUserId))
             .forEach(userId -> {
@@ -48,6 +68,7 @@ public class LocalAuthorityUserService {
                         CASE_TYPE,
                         caseId,
                         new UserId(userId));
+
 
                 } catch (Exception ex) {
                     logger.warn("Could not grant user {} access to case {}", userId, caseId, ex);
